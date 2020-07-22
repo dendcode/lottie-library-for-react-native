07abd2c67dabe87720c178810b45b1ed4a74011c
dendcode <dendcode@gmail.com>
2020-07-22 17:44:33 +0900

    donkey-kong game added

--- pages/step02.js
+++ pages/step02.js
@@ -3,37 +3,14 @@ import { StyleSheet, Text, View, Button, TouchableOpacity, AsyncStorage, TextInp
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LottieView from 'lottie-react-native';
+import DonkeyKong from "react-native-donkey-kong";
 
 
 
 export default function Step02({navigation}) {
 
+
+    return <DonkeyKong />;
     
-
-
-
-    return (
-        <View style={{flex:1}}>
-            
-                <View style={{flex:4, justifyContent: "flex-start", alignItems: "center"}}>
-                    <Text>Like Button</Text>
-                </View>
-                <View style={{flex:3, justifyContent: "center", alignItems: "center"}}>
-                <LottieView
-                    source={require('../assets/lottieJSON/shape-dance.json')}
-                    autoPlay
-                    loop={true}
-                    width= '100%'
-
-                />
-                </View>
-                <View style={{flex:3, justifyContent: "center", alignItems: "center"}}>
-                    <TouchableOpacity style={{backgroundColor: '#fff', width: '70%', height: '30%', borderRadius: 30, justifyContent: 'center', alignItems: 'center', marginTop: '20%'}} onPress={()=> navigation.navigate('Step03')}>
-                        <Text style={{fontSize: 35, color: '#3DC50D', fontWeight: 'bold'}}>Next</Text>
-                    </TouchableOpacity>
-                </View>
-
-        </View>
-    )
 
 }
